5edb426a96c23aa02fa694e9557eb38d4d9a35e2
uehara <shangyuanshouya@gmail.com>
2024-07-08 22:54:20 +0900

    Initial commit

---
+++ StudentManagement5/src/main/java/raisetech5/StudentManagement5/StudentManagement5Application.java
@@ -0,0 +1,26 @@
+package raisetech5.StudentManagement5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudentManagement5Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManagement5Application.class, args);
+  }
+
+  @GetMapping("/Name")
+  public String name() {
+    return "watasinonamaeha,taroudesu.";
+
+  }
+
+  
+
+
+
+}
